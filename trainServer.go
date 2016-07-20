37e3a549e7b7691a19088baccaebdb0d86a94b79
Rishad Omar <rishad@kaluma.com>
2016-07-20 22:05:20 +0200

    beginnings

--- trainServer.go
+++ trainServer.go
@@ -1,1 +1,31 @@
-package trainServer
+package main
+
+import (
+	"fmt"
+	"net/http"
+    "html"
+)
+
+const LocationTypeStation = "Station";
+const LocationTypePoint = "Point";
+
+type Location struct {
+    latitude float32
+    longitude float32
+    name string
+    locationType string
+}
+
+func handleGetNext(rw http.ResponseWriter, request *http.Request) {
+    fmt.Fprint(rw, "GetNext, %q", html.EscapeString(request.URL.Path))
+}
+
+func handlePostPosition(rw http.ResponseWriter, request *http.Request) {
+    fmt.Fprint(rw, "PostPosition , %q", html.EscapeString(request.URL.Path))
+}
+
+func main() {
+    http.HandleFunc("/next", handleGetNext)
+    http.HandleFunc("/position", handlePostPosition)
+    http.ListenAndServe(":8082", nil)
+}
